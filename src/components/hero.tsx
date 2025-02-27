beecf83e480dbcba5329b63f2566b1d2c0da4565
vishnuyvr <vishnuyvr18@gmail.com>
2025-02-26 18:37:22 -0600

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -43,7 +43,7 @@ export const Hero = () => {
         </h1>
         <p className={cn(styles.heroSubText, "mt-2 text-white-100")}>
         <TypingEffect /> <br />
-          ReactJS, NodeJS, TypeScript, MongoDB, ExpressJS, NextJS, ThreeJS and more.
+          ReactJS, NodeJS, TypeScript, JavaScript, Jenkins, Postman, Jira, ExpressJS and more.
         </p> 
         <p className={cn(styles.heroSubText, "mt-5 text-white-100")}>
           M.S in Computer Science from California State University, Fullerton. (Jan 2023 - Dec 2024)
